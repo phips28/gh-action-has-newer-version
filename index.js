ded49efe181e7ee8e5a2971bfc0882dc7420ecfd
phips28 <phips28@gmx.at>
2019-10-28 17:28:14 +0100

    fix branch

--- index.js
+++ index.js
@@ -9,28 +9,24 @@ Toolkit.run(async tools => {
 
   const currentVersion = pkg.version.toString()
 
-  console.log('arguments', tools.arguments)
-  console.log('context', tools.context)
-  console.log('opts', tools.opts)
   console.log('env', process.env)
-  console.log('payload', JSON.stringify(tools.payload))
-  const branch = tools.arguments.branch
-  // if (!branch) {
-  //   tools.exit.failure('`branch` needs to be set in `with` config')
-  //   return
-  // }
-  // console.log('branch', branch)
-  // // now go to the given branch to perform the same versioning
-  // await tools.runInWorkspace('git', ['checkout', branch])
-  //
-  // const pkg2 = tools.getPackageJSON()
-  // console.log('pkg2', pkg2)
-  // const branchVersion = pkg2.version.toString()
-  //
-  // console.log(currentVersion, branchVersion)
-  // if (compareVersions.compare(currentVersion, branchVersion, '>')) {
-  //   tools.exit.success(`current version is higher than in ${branch}. (${currentVersion} > ${branchVersion})`)
-  // } else {
-  //   tools.exit.failure(`current version must be higher than in ${branch}. (${currentVersion} > ${branchVersion})`)
-  // }
+  const branch = process.env.INPUT_BRANCH
+  if (!branch) {
+    tools.exit.failure('`branch` needs to be set in `with` config')
+    return
+  }
+  console.log('branch', branch)
+  // now go to the given branch to perform the same versioning
+  await tools.runInWorkspace('git', ['checkout', branch])
+
+  const pkg2 = tools.getPackageJSON()
+  console.log('pkg2', pkg2)
+  const branchVersion = pkg2.version.toString()
+
+  console.log(currentVersion, branchVersion)
+  if (compareVersions.compare(currentVersion, branchVersion, '>')) {
+    tools.exit.success(`current version is higher than in ${branch}. (${currentVersion} > ${branchVersion})`)
+  } else {
+    tools.exit.failure(`current version must be higher than in ${branch}. (${currentVersion} > ${branchVersion})`)
+  }
 })
